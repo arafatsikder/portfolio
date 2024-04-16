755f2a03f26a9dff3d638682495459056bfeae97
arafatsikder <arfatsikder929@gmail.com>
2024-04-16 09:49:42 -0700

    card information added

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -5,20 +5,24 @@ const Card = () => {
             <div className='all-cards'>
                 <div className='div-1'>
                     <div className='div1-style pl-6 px-6'>
-                        <h1 className='text-4xl font-semibold pt-2'>02.</h1>
-                        <p className='text-2xl h-color font-semibold py-2'>UX/UI DESIGN</p>
-                        <p className='text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus laoreet dolor metus, eu ullamcorper turpis ornare tincidunt. Vivamus tristique rhoncus enim. Curabitur varius est quis din pat.</p>
+                        <h1 className='text-4xl font-bold pt-2'>02.</h1>
+                        <p className='text-2xl h-color font-semibold py-2'>User-Friendly Interface</p>
+                        <p className='text-white'> A user-friendly interface makes it easy for visitors to navigate your website, find information quickly, and complete tasks efficiently. It enhances usability, reduces bounce rates, and increases conversions.</p>
                     </div>
                 </div>
                 <div className='div-2'>
-                    <h1 className='text-4xl'>01.</h1>
-                    <p className='text-3xl'>UX/UI DESIGN</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus laoreet dolor metus, eu ullamcorper turpis ornare tincidunt. Vivamus tristique rhoncus enim. Curabitur varius est quis din pat.</p>
+                    <div className='div2-style pl-6 px-6'>
+                        <h1 className='text-4xl font-bold pt-2 color2'>01.</h1>
+                        <p className='text-2xl h-color font-semibold py-2 text-black'>Responsive Design</p>
+                        <p className='text-white'>Responsive design ensures that your website adapts and displays properly on various devices, including desktops, laptops, tablets, and smartphones.It allows you to reach a wider audience and provides a consistent user experience.</p>
+                    </div>
                 </div>
                 <div className='div-3'>
-                    <h1 className='text-4xl'>03.</h1>
-                    <p className='text-3xl'>UX/UI DESIGN</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus laoreet dolor metus, eu ullamcorper turpis ornare tincidunt. Vivamus tristique rhoncus enim. Curabitur varius est quis din pat.</p>
+                    <div className='div3-style pl-6 px-6'>
+                        <h1 className='text-4xl font-bold pt-2'>03.</h1>
+                        <p className='text-2xl h-color font-semibold py-2'>Fast Loading Speed</p>
+                        <p className='text-white'>Fast loading speed is crucial for retaining visitors and improving search engine rankings. It enhances user experience by minimizing wait times and ensuring that content is delivered quickly.</p>
+                    </div>
                 </div>
             </div>
         </div>
