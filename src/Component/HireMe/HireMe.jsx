ecc75d24308050e3f0da693c9500436407a016f3
arafatsikder <arfatsikder929@gmail.com>
2024-04-21 09:24:28 -0700

    project done

---
+++ src/Component/HireMe/HireMe.jsx
@@ -0,0 +1,21 @@
+import { BiLogoTelegram } from "react-icons/bi";
+const HireMe = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <div><BiLogoTelegram /></div>
+                    <div>
+                        <p>HIRE ME FOR YOUR AWESOME PROJECT</p>
+                        <p></p>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HireMe;
